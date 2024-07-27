8f50c198ab4121f38011fce49274d41b43c24e17
the-anshu-dev <anshuchaurasia210@gmail.com>
2024-07-27 16:02:24 +0530

    first commit-2

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*', // Replace with the actual origin of your Next.js app
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, 
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Wallicon Api')
@@ -12,8 +17,14 @@ async function bootstrap() {
     .addTag('default')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-
+  SwaggerModule.setup('api', app, document, {
+    customCssUrl:
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
+    ],
+  });
 
 
 
